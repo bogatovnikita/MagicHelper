740c22dd99880ac80383a050d759810f9940b792
Nikita Bogatov <bogatovnikita@gmail.com>
2022-11-07 17:03:59 +0300

    add key

--- app/src/main/java/ar/cleaner/first/pf/ads/ads.kt
+++ app/src/main/java/ar/cleaner/first/pf/ads/ads.kt
@@ -2,7 +2,6 @@ package ar.cleaner.first.pf.ads
 
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import ar.cleaner.first.pf.BuildConfig
 import com.ads.library.AdsDelegate
 import com.ads.library.AdsManager
 import kotlinx.coroutines.Dispatchers
@@ -10,23 +9,23 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-fun Fragment.showAds(
-    onClosed: () -> Unit
-) {
-    lifecycleScope.launch(Dispatchers.Default) {
-        delay(1000)
-        withContext(Dispatchers.Main) {
-            AdsManager.showInterstitial(requireActivity(), object : AdsDelegate {
-                override fun adsClosed() {
-                    onClosed()
-                }
-            })
-        }
-    }
-}
+//fun Fragment.showAds(
+//    onClosed: () -> Unit
+//) {
+//    lifecycleScope.launch(Dispatchers.Default) {
+//        delay(1000)
+//        withContext(Dispatchers.Main) {
+//            AdsManager.showInterstitial(requireActivity(), object : AdsDelegate {
+//                override fun adsClosed() {
+//                    onClosed()
+//                }
+//            })
+//        }
+//    }
+//}
 
-fun Fragment.preloadAd() {
-    if (!AdsManager.checkAdsLoaded()) {
-        AdsManager.preloadAd(requireActivity(), BuildConfig.ADMOB_INTERSTITIAL)
-    }
-}
+//fun Fragment.preloadAd() {
+//    if (!AdsManager.checkAdsLoaded()) {
+//        AdsManager.preloadAd(requireActivity(), BuildConfig.ADMOB_INTERSTITIAL)
+//    }
+//}
