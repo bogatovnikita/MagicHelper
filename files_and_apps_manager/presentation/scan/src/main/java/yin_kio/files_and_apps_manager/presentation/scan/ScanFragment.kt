d89f31e0da9c50745448e4051f68410951ba3197
YinKio <jamycake@hotmail.com>
2023-05-11 18:29:33 +0300

    files_and_apps_manager: make up fam_fragment_scan.xml

---
+++ files_and_apps_manager/presentation/scan/src/main/java/yin_kio/files_and_apps_manager/presentation/scan/ScanFragment.kt
@@ -0,0 +1,4 @@
+package yin_kio.files_and_apps_manager.presentation.scan
+
+class ScanFragment {
+}
--- files_and_apps_manager/presentation/start/src/main/kotlin/yin_kio/files_and_apps_manager/presentation/start/StartFragment.kt
+++ files_and_apps_manager/presentation/start/src/main/kotlin/yin_kio/files_and_apps_manager/presentation/start/StartFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
 import by.kirich1409.viewbindingdelegate.viewBinding
+import file_manager.domain.server.FileAndAppsServerImpl
+import file_manager.domain.server.FileManagerServer
 import file_manager.start_screen.use_case.StartScreenUseCaseCreator
 import jamycake.lifecycle_aware.currentBackStackEntry
 import kotlinx.coroutines.CoroutineScope
@@ -20,6 +22,8 @@ class StartFragment : Fragment(R.layout.files_app_manager_start) {
 
     private val binding: FilesAppManagerStartBinding by viewBinding()
     private val viewModel: ViewModel by currentBackStackEntry { createViewModel(viewModelScope) }
+    // It is need for scan and overview screens.
+    private val fileManagerServer: FileManagerServer by currentBackStackEntry { FileAndAppsServerImpl() }
 
     private fun createViewModel(coroutineScope: CoroutineScope) : ViewModel{
         val context = requireContext().applicationContext
