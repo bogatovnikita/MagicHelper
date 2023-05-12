dd4130642df292acaaf572edf27313acf22b1c8d
YinKio <jamycake@hotmail.com>
2023-05-12 19:08:05 +0300

    files_and_apps_manager:data: add permissions requesting

--- files_and_apps_manager/presentation/src/main/kotlin/yin_kio/files_and_apps_manager/presentation/scan/FileManagerPermissionDialog.kt
+++ files_and_apps_manager/presentation/src/main/kotlin/yin_kio/files_and_apps_manager/presentation/scan/FileManagerPermissionDialog.kt
@@ -2,21 +2,35 @@ package yin_kio.files_and_apps_manager.presentation.scan
 
 import Yin_Koi.files_and_apps_manager.presentation.R
 import Yin_Koi.files_and_apps_manager.presentation.databinding.FilesAppManagerPermissionDialogBinding
+import android.Manifest
 import android.app.Dialog
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.permissions.requestManageExternalStorage
 import jamycake.lifecycle_aware.previousBackStackEntry
 
 class FileManagerPermissionDialog : DialogFragment(R.layout.files_app_manager_permission_dialog) {
 
     private val binding: FilesAppManagerPermissionDialogBinding by viewBinding()
     private val viewModel: ViewModel by previousBackStackEntry()
+
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ){
+        if (it.containsValue(false)){
+            viewModel.close()
+        } else {
+            viewModel.scan()
+        }
+    }
 
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -48,12 +62,29 @@ class FileManagerPermissionDialog : DialogFragment(R.layout.files_app_manager_pe
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             viewModel.command.collect{
                 when(it){
-                    Command.Close -> repeat(2){findNavController().navigateUp()}
-                    Command.RequestPermission -> TODO()
+                    Command.Close -> closeDialogAndScan()
+                    Command.RequestPermission -> {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            requireActivity().requestManageExternalStorage()
+                            closeDialogAndScan()
+                        } else {
+                            permissionLauncher.launch(
+                                arrayOf(
+                                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                                )
+                            )
+                        }
+                    }
+                    Command.ShowProgress -> findNavController().navigateUp()
                     else -> {}
                 }
             }
         }
+    }
+
+    private fun closeDialogAndScan() {
+        findNavController().popBackStack(R.id.startFragment, false)
     }
 
 }
