e66704e7781d9d45948d265ede5f682533aa9223
YinKio <jamycake@hotmail.com>
2023-05-19 13:59:31 +0300

    file_apps_manager:domain: fix file grouping

--- files_and_apps_manager/domain/scan_progress_use_case/src/main/java/file_manager/scan_progress/grouper/GrouperImpl.kt
+++ files_and_apps_manager/domain/scan_progress_use_case/src/main/java/file_manager/scan_progress/grouper/GrouperImpl.kt
@@ -4,6 +4,7 @@ import file_manager.domain.server.FileOrApp
 import file_manager.domain.server.GroupName
 import file_manager.domain.server.selectable_form.SelectableForm
 import file_manager.domain.server.selectable_form.SimpleSelectableForm
+import javax.swing.text.html.HTML.Tag.P
 import kotlin.io.path.Path
 import kotlin.io.path.extension
 
@@ -23,6 +24,7 @@ class GrouperImpl : Grouper {
 
         files.forEach {
             val extension = Path(it.id).extension
+            println("!!! ${extension}")
 
             when{
                 fileGroups.isAudio(extension) -> audio.add(it)
