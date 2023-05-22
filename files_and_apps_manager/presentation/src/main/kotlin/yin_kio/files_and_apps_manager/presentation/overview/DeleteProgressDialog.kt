ac1decec3949d0cecb26b7ac532f48cf14d4f2c2
YinKio <jamycake@hotmail.com>
2023-05-22 15:34:52 +0300

    file_apps_manager: fix deletion progress dialog cancelling

--- files_and_apps_manager/presentation/src/main/kotlin/yin_kio/files_and_apps_manager/presentation/overview/DeleteProgressDialog.kt
+++ files_and_apps_manager/presentation/src/main/kotlin/yin_kio/files_and_apps_manager/presentation/overview/DeleteProgressDialog.kt
@@ -1,10 +1,28 @@
 package yin_kio.files_and_apps_manager.presentation.overview
 
 import Yin_Koi.files_and_apps_manager.presentation.R
+import android.app.Dialog
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
 import yin_kio.files_and_apps_manager.presentation.dialog.FixedWidthDialogFragment
 
 class DeleteProgressDialog : FixedWidthDialogFragment(R.layout.dialog_deleting_progress) {
 
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return noCancelableDialog()
+    }
+
+    private fun noCancelableDialog() = object : Dialog(requireContext()) {
+        init {
+            setCancelable(false)
+            setCanceledOnTouchOutside(false)
+
+            window?.setBackgroundDrawable(ColorDrawable(android.R.color.transparent))
+        }
+
+        override fun onBackPressed() {}
+    }
 
 
 }
